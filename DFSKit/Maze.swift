0984ff3ec16e2968d12a4e1cbb2699f1df53a0d9
Aaron Sutton <aaronjsutton@icloud.com>
2018-06-18 23:54:40 -0400

    Initial commit 🎉

---
+++ DFSKit/Maze.swift
@@ -0,0 +1,83 @@
+//
+//  Maze.swift
+//  DFSKit
+//
+//  Created by Aaron Sutton on 6/18/18.
+//  Copyright © 2018 Aaron Sutton. All rights reserved.
+//
+
+import Foundation
+
+/// A structure that represents the underlying data for a grid based maze.
+public struct Maze {
+
+	/// Represents the two states of a maze space.
+	///
+	/// - passable: The space can be passed through in the game world (path).
+	/// - impassable: The space cannot be passed through (wall).
+	enum Space {
+		case passable
+		case impassable
+	}
+
+	/// The number of rows in the maze grid
+	public var rows: Int
+	/// The number of columns in the maze grid
+	public var columns: Int
+	private var grid: [[Space]]
+
+	/// Create a new maze, ungenerated.
+	///
+	/// - Parameters:
+	///   - rows: Number of rows in the maze.
+	///   - columns: Number of columns in the maze.
+	///
+	/// - Warning: Both `rows` and `columns` must be odd. If
+	/// 					 the values passed in are even, they will be incremented.
+	init(width columns: Int, height rows: Int) {
+		self.rows = rows % 2 != 0 ? rows : rows + 1
+		self.columns = columns % 2 != 0 ? columns : columns + 1
+		self.grid = Array(repeating: Array(repeating: .impassable,
+																			 count: columns),
+											count: rows)
+	}
+
+	/// Returns a row of spaces. This allows for
+	/// double bracket style subscript syntax.
+	///
+	/// - Parameter row: The row index.
+	subscript(row: Int, column: Int) -> Space {
+		assert(inBounds(row, column), "Index out of bounds")
+		return grid[row][column]
+	}
+
+	subscript(row: Int) -> [Space] {
+		assert(inBounds(row, 0), "Index out of bounds")
+		return grid[row]
+	}
+
+	private func inBounds(_ row: Int, _ column: Int) -> Bool {
+		return row >= 0 && row < rows && column >= 0 && column < columns
+	}
+
+}
+
+// MARK: - CustomStringConvertible
+extension Maze: CustomStringConvertible {
+	/// Prints the maze data using Unicode box drawing characters
+	public var description: String {
+		let border = String(repeatElement("━", count: columns * 2))
+		var box = "┏\(border)┓\n"
+
+		for row in 0...rows - 1 {
+			box += "┃"
+			for space in self[row] {
+				box += space == .passable ? "  " : "██"
+			}
+			box += ("┃\n")
+		}
+
+		box += "┗\(border)┛\n"
+		return box
+	}
+}
--- DFSKitTests/DFSKitTests.swift
+++ DFSKitTests/DFSKitTests.swift
@@ -10,27 +10,34 @@ import XCTest
 @testable import DFSKit
 
 class DFSKitTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
+	override func setUp() {
+		super.setUp()
+	}
+
+	override func tearDown() {
+		super.tearDown()
+	}
+
+	func testParity() {
+		let evenMaze = Maze(width: 20, height: 20)
+		XCTAssert(evenMaze.columns == 21 && evenMaze.rows == 21)
+		let oddMaze = Maze(width: 55, height: 55)
+		XCTAssert(oddMaze.columns == 55 && oddMaze.rows == 55)
+	}
+
+	func testGridCreate() {
+		let maze = Maze(width: 59, height: 59)
+		for row in 0...58 {
+			for col in 0...58 {
+				XCTAssert(maze[row, col] == .impassable)
+			}
+		}
+	}
+
+	func testString() {
+		let maze = Maze(width: 25, height: 9)
+		print(maze)
+	}
+
 }
