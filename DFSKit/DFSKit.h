49a131b50b458824c162c914419d656d6902da03
Aaron Sutton <aaronjsutton@icloud.com>
2018-06-18 20:39:43 -0400

    Initial Commit

---
+++ DFSKit/DFSKit.h
@@ -0,0 +1,19 @@
+//
+//  DFSKit.h
+//  DFSKit
+//
+//  Created by Aaron Sutton on 6/18/18.
+//  Copyright © 2018 Aaron Sutton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DFSKit.
+FOUNDATION_EXPORT double DFSKitVersionNumber;
+
+//! Project version string for DFSKit.
+FOUNDATION_EXPORT const unsigned char DFSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DFSKit/PublicHeader.h>
+
+
