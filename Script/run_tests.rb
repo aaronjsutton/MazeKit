77805a46a9e146f000cabe25cfe79d3b1e83ff14
Aaron Sutton <aaronjsutton@icloud.com>
2018-06-27 14:13:30 -0400

    Fix swift install

--- Script/run_tests.rb
+++ Script/run_tests.rb
@@ -1,7 +1,7 @@
 #!/usr/local/bin/ruby -w
 
-scheme = 'MazeKit'
-destination_name = 'iPhone 6'
+scheme = ''
+destination_name = ''
 
-command = "set -o pipefail && xcrun --toolchain swift xcodebuild -toolchain swift -scheme #{scheme} -destination 'name=#{destination_name}' test | xcpretty"
+command = ""
 exec command
